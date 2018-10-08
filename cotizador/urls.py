a613709cbdcb96050a6662fd3314fe84a6b6bd06
frinksdev <frinksdev@gmail.com>
2018-10-08 13:36:16 -0500

    route and template management

--- cotizador/urls.py
+++ cotizador/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from blog import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',views.post_list),
+
 ]
